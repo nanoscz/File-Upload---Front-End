662379dd03c1d7f33b6b125c31eb6230904e2098
fernandocto.scz <fernandocto.scz@gmail.com>
2019-05-05 19:07:01 -0400

    Add load and photo components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PhotoComponent } from './components/photo/photo.component';
+import { LoadComponent } from './components/load/load.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'photo', component: PhotoComponent},
+  {path: 'load', component: LoadComponent },
+  {path: '**', pathMatch: 'full', redirectTo: 'photo'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './components/photo/photo.component';
+import { LoadComponent } from './components/load/load.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    LoadComponent
   ],
   imports: [
     BrowserModule,
